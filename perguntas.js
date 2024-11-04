f36280a8aedbaf40088efa508265e3f94a447098
Paulo-Tullio <paulohenrynunes@gmail.com>
2024-11-04 07:58:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu Vidas Secas?',
+    'O brasileiro Graciliano Ramos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austria?',
+    'A capital da Áustria é Viena'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
